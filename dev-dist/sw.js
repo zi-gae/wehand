386b72d08fe43867ec7eea6e551585544e9a11b0
zi-gae <doscmDev@gmail.com>
2025-08-12 10:29:01 +0900

    chore: base url test

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kp9mr1p8b58"
+    "revision": "0.sbtse947p68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
