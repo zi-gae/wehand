3d92807237a3d5a2cdaf73032d51298b4c475563
zi-gae <doscmDev@gmail.com>
2025-08-13 02:18:18 +0900

    누락 safe 적용

--- src/pages/MatchDetailPage.tsx
+++ src/pages/MatchDetailPage.tsx
@@ -483,7 +483,7 @@ const MatchDetailPage = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className={`min-h-screen ${theme.background.tennis} page-content transition-colors duration-300`}
+      className={`min-h-screen ${theme.background.tennis} page-content pb-safe transition-colors duration-300`}
     >
       {/* Header */}
       <motion.header
